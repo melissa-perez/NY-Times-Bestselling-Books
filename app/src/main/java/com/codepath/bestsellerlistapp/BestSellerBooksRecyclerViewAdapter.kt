34e3c283834592b14970cb2b484b7ca78980f078
Melissa Perez <melmperz@gmail.com>
2023-03-09 13:19:20 -0500

    Finish stretch features

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -1,11 +1,14 @@
 package com.codepath.bestsellerlistapp
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.R.id
@@ -64,6 +67,11 @@ class BestSellerBooksRecyclerViewAdapter(
             holder.mItem?.let { book ->
                 mListener?.onItemClick(book)
             }
+        }
+
+        holder.mBookBuy.setOnClickListener{
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(book.amazonUrl))
+            startActivity(it.context, browserIntent, null)
         }
     }
 
